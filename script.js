b72a0112b7e98085321a7e6f52acfb20a4ce8483
Bharvi-Sampat <bharvi.sampat@gmail.com>
2022-08-09 22:06:51 +0530

    second commit

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ $(window).on("load", function () {
     $(this).addClass("active");
   });
 });
+
+//
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.querySelector(".navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.remove("sticky");
+  } else {
+    navbar.classList.add("sticky");
+  }
+}
