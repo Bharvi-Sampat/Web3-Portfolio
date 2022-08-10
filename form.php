40a94f87c375a93b60f3ab6bff3f56e31d697ae1
Bharvi-Sampat <bharvi.sampat@gmail.com>
2022-08-10 14:33:12 +0530

    sixth commit

---
+++ form.php
@@ -0,0 +1,16 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$subject = $_POST['subject'];
+$to = "bharvi.sampat@gmail.com";
+$txt ="Name: ". $name . "\r\nEmail: " . $email . "\r\nMessage: " . $message;
+$headers = "From: PankajDalmiaWebsite";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html#get-in-touch");
+?>
